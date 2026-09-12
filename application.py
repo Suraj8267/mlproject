02d84264ad0274eadffb4203841f78649979620c
Suraj8267 <singhsuraj182005@gmail.com>
2026-09-12 20:30:57 +0530

    Deployment Phase

--- application.py
+++ application.py
@@ -22,9 +22,9 @@ def predict_datapoint():
         return render_template('home.html')
     else:
         data = CustomData(
-            gender = request.form['gender'],
-            race_ethnicity= request.form['race_ethnicity'],
-            parental_level_of_education= request.form['parental_level_of_education'],
+            gender = request.form.get('gender'),
+            race_ethnicity= request.form.get('race_ethnicity'),
+            parental_level_of_education= request.form.get('parental_level_of_education'),
             lunch= request.form.get('lunch'),
             test_preparation_course= request.form.get('test_preparation_course'),
             reading_score= float(request.form.get('reading_score')),
